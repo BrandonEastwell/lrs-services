ad2cd4855ff039e7e6071b95fee5864a105d3efb
BrandonEastwell <brandoneastwell@yahoo.com>
2025-03-06 03:12:07 +0000

    updated project structure

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import App from '../App.jsx';
+import App from '../app/App.tsx';
 
 describe('App', () => {
     it('renders headline', () => {
