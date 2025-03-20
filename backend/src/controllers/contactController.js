98cf2a75aa68f582d9d8f7b10d32e155e504dbdf
BrandonEastwell <brandoneastwell@yahoo.com>
2025-03-20 02:09:11 +0000

    backend rest api logic for handling post request to contact route, uses nodemailer to send data to business email

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -1,4 +1,5 @@
 import { body, validationResult} from "express-validator";
+import nodemailer from "nodemailer";
 
 const validated = [
     body("name").trim().notEmpty().withMessage("Name can not be empty.").escape(),
@@ -7,12 +8,38 @@ const validated = [
     body("extra").trim().escape()
 ]
 
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: globalThis.process.env.EMAIL,
+        pass: globalThis.process.env.EMAILPASS
+    }
+})
+
 function sendEmail(req, res, next) {
     const { name, email, phone, extra, service } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: globalThis.process.env.EMAIL,
+        subject: `${name} + " " + ${service}`,
+        text: `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nService: ${service}\n\nMessage:\n${extra}`
+    }
 
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) return res.status(400).send(errors.array());
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error);
+                return next(error);
+            } else {
+                console.log("Email sent: " + info.response);
+            }
+        })
+
+        //res.send("Email sent successfully!");
         res.redirect("/");
     } catch (e) {
         next(e);
